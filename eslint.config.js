c1a0575c1129cd951ac3ce8d7c9df4baa818af79
NMRSantos <170880035+NMRSantos@users.noreply.github.com>
2026-03-16 23:48:17 +0000

    Add eslint-config-prettier and respective configuration

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   { ignores: ["**", "!src/**"] },
@@ -13,4 +14,5 @@ export default defineConfig([
   { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  eslintConfigPrettier,
 ]);
